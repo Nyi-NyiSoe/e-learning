cd9228d6884f60765fc235373f2fd2898c1605b0
Nyi Nyi Soe <jackemrick2k@gmail.com>
2024-01-23 15:44:57 +0630

    added question card

--- lib/screens/quiz_question_page.dart
+++ lib/screens/quiz_question_page.dart
@@ -7,6 +7,7 @@ class QuizQuestion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         title: Text('Quizz'),
         actions: [ElevatedButton(onPressed: () {}, child: const Text('Skip'))],
@@ -17,20 +18,66 @@ class QuizQuestion extends StatelessWidget {
           ProgressBar(),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text.rich(TextSpan(
-                text: "Question 1",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium!
-                    .copyWith(color: Colors.grey),
-                children: [TextSpan(text: "/10",
-                style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Colors.grey)
-                ),
-                
-                ]),
-                ),
+            child: Text.rich(
+              TextSpan(
+                  text: "Question 1",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium!
+                      .copyWith(color: Colors.grey),
+                  children: [
+                    TextSpan(
+                        text: "/10",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(color: Colors.grey)),
+                  ]),
+            ),
           ),
-          const Divider(thickness: 1.5,)
+          const Divider(
+            thickness: 1.5,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(25)),
+            child: Column(
+              children: [
+                Text(
+                  'Veniam ex sunt labore minim cillum reprehenderit adipisicing commodo. ',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(color: Colors.black),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 25),
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      border: Border.all(color: Colors.grey)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '1, Test',
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                      Container(
+                        width: 26,
+                        height: 26,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          border: Border.all(color: Colors.grey)
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
