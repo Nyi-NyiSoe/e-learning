548cd63cb2ddebf33211014c3d5d57a97c0fb6ec
Nyi Nyi Soe <jackemrick2k@gmail.com>
2024-03-09 23:05:16 +0700

    refractored setting Page

--- lib/screens/setting_page.dart
+++ lib/screens/setting_page.dart
@@ -1,99 +1,90 @@
-import 'package:edulearn/utils/user_data.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SettingPage extends StatefulWidget {
+class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
-  State<SettingPage> createState() => _SettingPageState();
-}
-
-late dynamic data;
-
-@override
-void initState() async {
-  data = await UserData().getUserData();
-  print(data);
-}
-
-class _SettingPageState extends State<SettingPage> {
-  @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          //backgroundColor: Colors.transparent,
-          actions: [Icon(Icons.logout)],
-        ),
-        body: Column(
-          children: [
-            Stack(children: [
-              Column(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(30),
-                            topLeft: Radius.circular(30))),
-                  ),
-                ],
-              ),
-              Positioned(
-                top: 100,
-                left: 50,
-                right: 50,
-                child: Center(
-                  child: Card(
-                    child: Container(
-                      width: 300,
-                      height: 150,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 50,
-                left: 50,
-                right: 50,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+        child: Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+                child: Stack(
                   children: [
-                    CircleAvatar(
-                      radius: 50,
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Icon(Icons.logout)),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Text('data'),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: ListTile(
-                            leading: Text('Currently learning:'),
-                            trailing: Text('6 courses'),
-                          ),
-                        )
-                      ],
-                    )
+                    Positioned(
+                        top: 10,
+                        left: 10,
+                        child:
+                            Text('My Profile', style: TextStyle(fontSize: 25))),
                   ],
                 ),
               ),
-            ]),
-              TextButton(onPressed: () {}, child: Text('Get data'))
-          ],
-        ),
+            ],
+          ),
+           
+          Positioned(
+            top: 120,
+            left: 37,
+            right: 37,
+            child: Stack(
+              children: [
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Container(
+                    height: 150,
+                    width: 320,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Name',style: Theme.of(context).textTheme.headlineMedium,),
+                          ListTile(
+                            leading: Text('Currently learning:'),
+                            trailing: Text('10 Courses'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+               
+              ],
+            ),
+          ),
+          Positioned(
+                  top: 55,
+                  left:37,
+                  right: 37,
+                  child: CircleAvatar(
+                    radius: 55,
+                  ),
+                ),
+        ],
       ),
-    );
+    ));
   }
 }
